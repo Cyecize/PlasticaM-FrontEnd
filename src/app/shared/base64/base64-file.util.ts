de4a827304c402853be174067967d7b7cfcd535a
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-04-05 00:03:25 +0300

    Fix function for picking content type out of base64.

--- src/app/shared/base64/base64-file.util.ts
+++ src/app/shared/base64/base64-file.util.ts
@@ -2,7 +2,7 @@ import {Base64File} from './base64-file';
 
 export class Base64FileUtil {
   public static createBase64File(base64: string, fileName: string): Base64File {
-    const contentType = base64.split(':')[0].replace('data:', '');
+    const contentType = base64.split(';')[0].replace('data:', '');
     return {fileName, contentType, base64};
   }
 }
