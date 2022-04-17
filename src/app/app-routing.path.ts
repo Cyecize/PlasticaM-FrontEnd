a3f23ce1f2b289109de23aca5c7f12fae8298a51
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-04-17 21:03:04 +0300

    Rename route for product details to avoid conflict with BE endpoint.

--- src/app/app-routing.path.ts
+++ src/app/app-routing.path.ts
@@ -7,7 +7,7 @@ export class AppRoutingPath {
   public static readonly ABOUT_US: RouteConfig = new RouteConfig('about-us', null);
   public static readonly PRODUCTS: RouteConfig = new RouteConfig('products', null);
   public static readonly PRODUCTS_CATEGORY: RouteConfig = new RouteConfig('category/:catId', AppRoutingPath.PRODUCTS);
-  public static readonly PRODUCT_DETAILS: RouteConfig = new RouteConfig('products/:prodId', null);
+  public static readonly PRODUCT_DETAILS: RouteConfig = new RouteConfig('product/:prodId', null);
   public static readonly LOGIN: RouteConfig = new RouteConfig('login', null);
   public static readonly PRIVACY_POLICY: RouteConfig = new RouteConfig('privacy-policy', null);
   public static readonly ADMIN_PANEL: RouteConfig = new RouteConfig('admin-panel', null);
